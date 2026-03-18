8fceda043db024733da421fa73919f24b0fcaede
leo <leo.latu@ecole-se.org>
2026-03-18 11:39:23 +0100

    creation du dossier .github et changement du storage account name en "eval"

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "leo_final_eval_rg" {
 }
 
 resource "azurerm_storage_account" "leo_final_eval_rg" {
-  name                     = "btsciel2finaleval"
+  name                     = "eval"
   resource_group_name      = azurerm_resource_group.leo_final_eval_rg.name
   location                 = azurerm_resource_group.leo_final_eval_rg.location
   account_tier             = "Standard"
