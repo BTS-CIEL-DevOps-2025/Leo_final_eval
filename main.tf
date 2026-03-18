b3fc3740002603ed4687298a061693ad93a016bc
leo <leo.latu@ecole-se.org>
2026-03-18 11:53:43 +0100

    changement du ressources group name

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "leo_final_eval_rg" {
+resource "azurerm_resource_group" "resources_partagees" {
   name     = "leo_final_eval_rg"
   location = "France Central"
 }
 
 resource "azurerm_storage_account" "leo_final_eval_rg" {
   name                     = "btsciel2finaleval"
-  resource_group_name      = azurerm_resource_group.leo_final_eval_rg.name
+  resource_group_name      = azurerm_resource_resources_partagees.name
   location                 = azurerm_resource_group.leo_final_eval_rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
